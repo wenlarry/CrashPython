f01b0c5174512736a0fe9334054888d5d2104f21
wenlarry <wenlarry@icloud.com>
2018-02-11 23:56:33 +0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,23 @@
+
+#  list comprehension
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+#  looping thru a slice
+
+# Python list tru only the first three names
+players =['ch', 'ma', 'mi', 'fl', 'el']
+print("Here are the first 3 players:")
+for player in players[:3]:
+	print(player.title())
+
+
+
+
+
+
+
+
+
+
+
---
+++ list_number.py
@@ -0,0 +1,12 @@
+#  Using range to make a list of numbers
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+
+
+
+
+
+
+
+
